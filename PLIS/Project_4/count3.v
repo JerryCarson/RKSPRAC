45da4e6d0448678ccad83781629c31b267eb0400
Arvidvittenberg <alexneiman@yandex.ru>
2021-07-20 16:26:30 +0300

    dhdhj

--- PLIS/Project_4/count3.v
+++ PLIS/Project_4/count3.v
@@ -1,38 +1,36 @@
-/*module counter(CLK, LED0, LED1, LED2);
-input wire CLK;
-output wire LED0, LED1, LED2;
+//module counter(CLK, LED0, LED1, LED2);
+//input wire CLK;
+//output wire LED0, LED1, LED2;
+//
+//reg LED;
+//initial LED <= 1'b0;
+//
+//// Счётчик
+//reg [24:0] counter;
+//initial counter <= 25'd0;
+//
+//always @(posedge CLK) begin
+//	if (counter == 25'd25000000) begin
+//		counter <= 25'd0;
+//		LED <= -LED;
+//	end else begin
+//		counter <= counter + 1'b1;
+//	end
+//end
+//assign LED0 = LED;
+//assign LED1 = -LED;
+//assign LED2 = LED;
+//
+//endmodule 
 
-reg LED;
-initial LED <= 1'b0;
-
-// Счётчик
-reg [24:0] counter;
-initial counter <= 25'd0;
-
-always @(posedge CLK) begin
-	if (counter == 25'd25000000) begin
-		counter <= 25'd0;
-		LED <= -LED;
-	end else begin
-		counter <= counter + 1'b1;
-	end
-end
-assign LED0 = LED;
-assign LED1 = -LED;
-assign LED2 = LED;
-
-endmodule */
-
-module count3(clk, s, m);
-input wire clk;
-//input wire m;
-output reg [10:0] s;
+module count3(clk, m, s, sum);
+input clk;
 input [10:0] m;
+input [10:0] s;
+output [10:0] sum;
 
-reg [10:0] count;
-initial count <= 11'd0;
 always @(posedge clk) begin
-	count <= count + m;
+	assign sum = s + m;
 end
 
 endmodule 
