a1bf1b5408e5b925ddd09cb6d0e3bb4998780ac0
neyman <alexneiman@yandex.ru>
2021-07-22 21:24:51 +0300

    .

--- PLIS/Project_4/keep.v
+++ PLIS/Project_4/keep.v
@@ -1,39 +1,42 @@
-module keep (a, out, LED0, LED1, LED2);
-output LED0, LED1, LED2;
-input [1:0] a;
-output reg [10:0] out;
+module keep (
+output reg LED0, LED1, LED2,
+input [1:0] a,
+output reg [10:0] out,
 
-reg LED;
-initial LED <= 1'b0; 
-
+output reg LED
+ );
+ 
+initial 
+LED <= 1'b0;
+ 
 always @(a) begin
 	case(a)
 	
 	11'd0: begin
 	out = 5;
-	assign LED0 = LED;
-	assign LED1 = - LED;
-	assign LED2 = - LED;
+	LED0 = LED;
+	LED1 = ~ LED;
+	LED2 = ~ LED;
 	end
 	
 	11'd1: begin
 	out = 10;
-	assign LED0 = - LED;
-	assign LED1 = LED;
-	assign LED2 = - LED;
+	LED0 = ~ LED;
+	LED1 = LED;
+	LED2 = ~ LED;
 	end
 	
 	11'd2: begin
-	assign LED0 = - LED;
-	assign LED1 = - LED;
-	assign LED2 = LED;
+	LED0 = ~ LED;
+	LED1 = ~ LED;
+	LED2 = LED;
 	end
 	
 	11'd3: begin
 	out = 20;
-	assign LED0 = LED;
-	assign LED1 = LED;
-	assign LED2 = - LED;
+	LED0 = LED;
+	LED1 = LED;
+	LED2 = ~ LED;
 	end
 	
 	endcase
